7c424515d99b54f395513d912b83fb5872581f14
Aaron Simmons <asimmons@sensoryinc.com>
2016-01-04 17:46:10 -0700

    fleshing out dsl

--- plugin/src/main/groovy/org/paleozogt/republish/RepublishPlugin.groovy
+++ plugin/src/main/groovy/org/paleozogt/republish/RepublishPlugin.groovy
@@ -2,8 +2,23 @@ package org.paleozogt.republish
 
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.artifacts.Configuration
 
 class RepublishPlugin implements Plugin<Project> {
     void apply(Project project) {
-    }    
+        project.extensions.create("republish", RepublishExtension)
+
+        project.task('republish') << {
+            logger.lifecycle("republish {}", project.republish.groupIncludes)
+            project.republish.configs.each { config ->
+                logger.lifecycle("{}", config)
+            }
+        }
+    }
+}
+
+class RepublishExtension {
+    Configuration[] configs
+    String[] groupIncludes
+    String[] groupExcludes
 }
